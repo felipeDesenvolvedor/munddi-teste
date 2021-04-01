24148b9373c9758720511bceaaf92cd5a19b5b20
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2021-04-01 18:12:55 -0300

    Update API.js

--- mapareact/src/API.js
+++ mapareact/src/API.js
@@ -1,12 +1,18 @@
 import {Loader} from "./utils"
 
 const regionBoundaries = map => {
-	let latNordeste = map.getBounds().Ra.i
-	let lngNordeste = map.getBounds().La.i
-	let latSuldoeste = map.getBounds().Ra.g
-	let lngSuldoeste = map.getBounds().La.g
-    
-	return {latNordeste, lngNordeste, latSuldoeste, lngSuldoeste}
+	let latNordeste; let lngNordeste; let latSuldoeste; let lngSuldoeste; 
+
+var obj1 = Object.keys(map.getBounds())[0]
+
+var obj2 = Object.keys(map.getBounds())[1]
+
+ latNordeste = map.getBounds()[obj1].i
+ lngNordeste = map.getBounds()[obj1].i
+ latSuldoeste = map.getBounds()[obj2].g
+ lngSuldoeste = map.getBounds()[obj2].g
+
+return {latNordeste, lngNordeste, latSuldoeste, lngSuldoeste}
 }
 
 const fetchStoresInTheRegion = map => {
